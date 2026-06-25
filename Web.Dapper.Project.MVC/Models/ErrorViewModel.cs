1bad8837c017bf2356acc2261502711f956b6f5e
Faruk ÖZDURAN <farukfurkan.ozduran@outlook.com>
2026-06-25 13:45:43 +0300

    Add project files.

---
+++ Web.Dapper.Project.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web.Dapper.Project.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
